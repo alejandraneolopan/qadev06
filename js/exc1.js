f609ed652f61bb0529f3422461b376a6e69c78c8
Alejandra Neolopan <alejandra.neolopan@fundacion-jala.org>
2016-01-25 12:36:47 -0400

    Practice 1 - Adding a class and methods

--- js/exc1.js
+++ js/exc1.js
@@ -1,17 +1,94 @@
 /**
- * Created by alejandraneolopan on 1/19/2016.
+ * @Class: MathNumbers
+ * @Description: Contain a set of numbers, it contains the methods: Maximum, minimum, avg,sum over whole set
+ * @Created: alejandraneolopan
+ * @Date: 1/19/2016.
  */
-var operations=function(parameters){
-    //Max, min, average, total
-    //var sum=0;
-    //parameters.length
-     console.log('MAX is ',getMaximum(arguments,0));
-    console.log('MIN is ',getMinimum(arguments,0));
-    console.log('AVG is ',getAvg(arguments));
-    console.log('SUM is ',getSum(arguments,0));
 
+ var MathNumbers=function(){//arguments n, no hay forma de decir varios...
+    //Properties
+    this.numbers=arguments;
+    //Methods
+    this.doOpe=function(){
+        
+        if (arguments.length>0)
+        {
+         //If there is arguments mathe.doOpe(1,2,3)   
+            this.doSum.apply(this, arguments);
+            this.doAvg.apply(this, arguments);
+            this.getMaximum.apply(this, arguments);
+            this.getMinimum.apply(this, arguments);
+        }
+        else
+        {//If the object call mathe=new MatNumbers(1,2,3); mathe.doOpe()
+            if (this.numbers.length>0)
+            {
+            //Display all operations in console
+            this.doSum();
+            this.doAvg();
+            this.getMaximum();
+            this.getMinimum();
+            }
+            else
+                {console.log('There is not numbers for the operations');}
+        }
+    };
+
+    this.doSum=function()
+    {
+       
+        if (arguments.length>0)
+        {console.log('SUM is ',getSum(secondParam,0));}
+        else
+        {
+             if (this.numbers.length>0)
+            {console.log('SUM is ',getSum(this.numbers,0));}
+            else
+                {console.log('There is not numbers for the sum');}
+        }
+    };
+   
+    this.doAvg=function()
+    {
+        if (arguments.length>0)
+        {console.log('AVG is ',getAvg(secondParam));
+        }
+        else
+        {
+            if (this.numbers.length>0)
+            {console.log('AVG is ',getAvg(this.numbers));}
+            else
+                {console.log('There is not numbers for the Average.');}
+        }
+    };
+    this.getMaximum=function()
+    {
+        if (arguments.length>0)
+        {console.log('MAX is ',getMaximum(secondParam,0));}
+        else
+        {
+            if (this.numbers.length>0)
+            {console.log('MAX is ',getMaximum(this.numbers,0));}
+            else
+                {console.log('There is not numbers for the Maximum.');}
+        }
+    };
+    this.getMinimum=function()
+    {
+        if (arguments.length>0)
+        {console.log('MIN is ',getMinimum(secondParam,0));}
+        else
+        {
+            if (this.numbers.length>0)
+            {
+            console.log('MIN is ',getMinimum(this.numbers,0));}
+            else
+                {console.log('There is not numbers for the Minimum.');}
+        }
+    };
 };
-
+//Methods independent
+//Maximum using recursive methods
 var getMaximum=function(params,n)
 {
     if(!params[n])
@@ -25,7 +102,8 @@ var getMaximum=function(params,n)
         else
         {return aux;}
     }
-}
+};
+//Minimum using recursive methods
 var getMinimum=function(params,n)
 {
     if(!params[n])
@@ -39,11 +117,12 @@ var getMinimum=function(params,n)
         {return aux;}
     }
 };
+//Average
 var getAvg=function(params)
 {
     return getSum(params,0)/params.length;
 };
-
+//Sum using recursive methods
 var getSum=function(params,n)
 {
     if (!params[n])
@@ -54,5 +133,5 @@ var getSum=function(params,n)
     }
 };
 
-
-operations(5,4,3,2);
+var mathe=new MathNumbers();
+mathe.doOpe(1,2,3);
